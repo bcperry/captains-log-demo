c77ba7fe444a61890db3a791272547424bd02280
Blaine Perry <blainecperry@gmail.com>
2026-01-09 09:17:44 -0600

    feat: [US-005] - Azure Speech Services integration

---
+++ app/speech/__init__.py
@@ -0,0 +1,27 @@
+"""Azure Speech Services integration module.
+
+This module provides integration with Azure Speech Services SDK,
+supporting both Azure Commercial and Azure Government cloud environments.
+"""
+
+from speech.client import (
+    SpeechClient,
+    SpeechConfig,
+    SpeechConfigurationError,
+    SpeechServiceError,
+    SpeechServiceUnavailableError,
+    SpeechRecognitionError,
+    clear_speech_client_cache,
+    get_speech_client,
+)
+
+__all__ = [
+    "SpeechClient",
+    "SpeechConfig",
+    "SpeechConfigurationError",
+    "SpeechServiceError",
+    "SpeechServiceUnavailableError",
+    "SpeechRecognitionError",
+    "clear_speech_client_cache",
+    "get_speech_client",
+]
