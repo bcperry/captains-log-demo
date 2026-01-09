230763bdcb01344a3ea0cadaaf1ec285e2c3badc
Blaine Perry <blainecperry@gmail.com>
2026-01-09 09:22:46 -0600

    feat: [US-006] - Audio transcription endpoint

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,7 +1,9 @@
 """API routers for the Captain's Log FastAPI application."""
 
 from api.auth import router as auth_router
+from api.transcribe import router as transcribe_router
 
 __all__ = [
     "auth_router",
+    "transcribe_router",
 ]
